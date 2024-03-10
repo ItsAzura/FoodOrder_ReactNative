0b0cd1dc97c15e7bdd4779b85b848584cdc3ba3e
ItsAzura <azurawithluz@gmail.com>
2024-03-10 16:42:18 +0700

    fixing ui issues

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -6,11 +6,22 @@ import Colors from "@/constants/Colors";
 import products from "@/assets/data/products";
 
 import { Image } from "react-native";
+import { Product } from "@/types";
 
-const ProductListItem = ({ product }: { product: any }) => {
+export const defaultPizzaImg =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.img} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImg }}
+        style={styles.img}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
