4a6c5fb4bc12a4ed0e0ca41f0a5d3ca4bb6f3472
ItsAzura <azurawithluz@gmail.com>
2024-03-10 20:16:31 +0700

    Some Note P1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,18 @@
 import { FlatList, StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
 import products from "@/assets/data/products";
-
-import { Image } from "react-native";
 import ProductListItem from "@/components/ProductListItem";
 
 export default function MenuScreen() {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10 }}
-      columnWrapperStyle={{ justifyContent: "space-between" }}
+    <FlatList //Một component hiển thị danh sách dữ liệu
+      data={products} //chứa dữ liệu của danh sách cần hiển thị
+      renderItem={
+        //Props này xác định cách mỗi phần tử trong data được hiển thị.
+        ({ item }) => <ProductListItem product={item} /> //Cho biết rằng mỗi phần tử của danh sách (item) sẽ được truyền vào component ProductListItem thông qua props product
+      }
+      numColumns={2} //Props này xác định số cột trong danh sách
+      contentContainerStyle={{ gap: 10, padding: 10 }} //Props này cho phép bạn cấu hình kiểu dáng của nội dung chính của FlatList
+      columnWrapperStyle={{ justifyContent: "space-between" }} //Props này xác định kiểu dáng của mỗi dòng trong danh sách khi numColumns lớn hơn 1.
     />
   );
 }
