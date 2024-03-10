55d24f8617b05bff449b87cb2b71262d100daabd
ItsAzura <azurawithluz@gmail.com>
2024-03-10 15:52:29 +0700

    Building Ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,19 @@ import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import products from "@/assets/data/products";
+
+const product = products[0];
+
+import { Image } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image}} style = {styles.img} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
@@ -20,16 +22,25 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    padding: 10,
+    borderRadius: 10,
+  },
+  img: {
+    width: "100%",
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    marginVertical: 10,
+    marginHorizontal: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  price: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.light.tint,
+    marginHorizontal: 10,
   },
+  
 });
