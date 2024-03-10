eb37439b4d4772069f201c1e93c7d1e11a6f9fab
ItsAzura <azurawithluz@gmail.com>
2024-03-10 20:44:30 +0700

    Create another screen with Expo Router

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,30 +1,32 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
+import { Pressable, StyleSheet } from "react-native";
 import { Text, View } from "@/components/Themed";
 import Colors from "@/constants/Colors";
-import products from "@/assets/data/products";
-
 import { Image } from "react-native";
 import { Product } from "@/types";
+import { Link } from "expo-router";
 
 export const defaultPizzaImg =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
+//kiểu dữ liệu đại diện cho các props mà một component ProductListItem cần nhận.
 type ProductListItemProps = {
-  product: Product;
+  product: Product; //product: Đại diện cho thông tin của một sản phẩm và có kiểu dữ liệu là Product
 };
 
+//Định nghĩa một functional component ProductListItem, nhận props product thông qua destructuring.
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImg }}
-        style={styles.img}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImg }} //Có img thì product.image ko thì defaultPizzaImg
+          style={styles.img}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -36,7 +38,7 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     padding: 10,
     borderRadius: 10,
-    margin: 10,
+    maxWidth: "50%",
   },
   img: {
     width: "100%",
