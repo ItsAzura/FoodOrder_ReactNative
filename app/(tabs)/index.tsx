4daf0d7b642a654a0253f18d8024e6b20670066d
ItsAzura <azurawithluz@gmail.com>
2024-03-10 16:33:05 +0700

    custom componet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,45 +2,15 @@ import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
-import Colors from "@/constants/Colors";
 import products from "@/assets/data/products";
 
-const product = products[0];
-
 import { Image } from "react-native";
+import ProductListItem from "@/components/ProductListItem";
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image}} style = {styles.img} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View style={{ flex: 1 }}>
+      <ProductListItem product={products[5]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "flex-start",
-    padding: 10,
-    borderRadius: 10,
-  },
-  img: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical: 10,
-    marginHorizontal: 10,
-  },
-  price: {
-    fontSize: 18,
-    fontWeight: "600",
-    color: Colors.light.tint,
-    marginHorizontal: 10,
-  },
-  
-});
---
+++ components/ProductListItem.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import products from "@/assets/data/products";
+
+import { Image } from "react-native";
+
+const ProductListItem = ({ product }: { product: any }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.img} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "flex-start",
+    padding: 10,
+    borderRadius: 10,
+  },
+  img: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.light.tint,
+    marginHorizontal: 10,
+  },
+});
